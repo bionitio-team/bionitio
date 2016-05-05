02a59b0225873fdf26723d1661c61a86577093e9
David Powell <david@drp.id.au>
2016-05-05 17:22:16 +1000

    Add testing of --minlen

--- .travis/test.sh
+++ .travis/test.sh
@@ -10,11 +10,25 @@ $exe -h | grep -q 'Usage:'|| {
     let errors+=1
 }
 
+
 res1='test_data/one_sequence.fasta	1	237	237	237	237'
 $exe test_data/one_sequence.fasta | grep -q "$res1" || {
-    echo "Test Failed.  Expected '$res1'"
+    echo "Test Failed $exe.  Expected '$res1'"
     let errors+=1
 }
+
+res2='test_data/two_sequence.fasta	2	357	120	178	237'
+$exe test_data/two_sequence.fasta | grep -q "$res2" || {
+    echo "Test Failed $exe.  Expected '$res2'"
+    let errors+=1
+}
+
+res3='test_data/two_sequence.fasta	1	237	237	237	237'
+$exe --minlen 200 test_data/two_sequence.fasta | grep -q "$res3" || {
+    echo "Test Failed $exe.  Expected '$res3'"
+    let errors+=1
+}
+
 
 
 [ "$errors" -gt 0 ] && {

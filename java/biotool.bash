f8fc95709d6c1d59c03c04ebd5bca753d96e2297
Andrew Lonsdale <andrew.lonsdale@lonsbio.com.au>
2016-05-06 11:22:46 +1000

    Clean up paths

--- java/biotool.bash
+++ java/biotool.bash
@@ -3,10 +3,12 @@
 # Wrapper around jar file
 # TODO: Change memory handling
 
+JAR_LOC="$(dirname $0)/biotool/target/biotool-1.0-jar-with-dependencies.jar"
+
 if [ $# -eq 0 ]
  then
-java -Xmx512m -jar $(dirname $0)/biotool/target/biotool-1.0-jar-with-dependencies.jar </dev/stdin
+java -Xmx512m -jar $JAR_LOC </dev/stdin
  else
-java -Xmx512m -jar $(dirname $0)/biotool/target/biotool-1.0-jar-with-dependencies.jar "$*"
+java -Xmx512m -jar $JAR_LOC "$*"
 fi
 

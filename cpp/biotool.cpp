d389db3f9c5b011d8c29165e7c27ec5759f0a5a5
Bernie Pope <bjpope@unimelb.edu.au>
2016-05-10 18:11:35 +1000

    Update command line argument processing

--- cpp/biotool.cpp
+++ cpp/biotool.cpp
@@ -1,20 +1,37 @@
 #include <iostream>
-
 #include <seqan/arg_parse.h>
+
+#define DEFAULT_MIN_LEN 0
+
+struct BiotoolOptions
+{
+    unsigned minlen;
+    bool verbose;
+    bool version;
+    //seqan::CharString text;
+
+    BiotoolOptions() :
+    minlen(DEFAULT_MIN_LEN), verbose(false), version(false)
+    {}
+};
 
 int main(int argc, char const ** argv)
 {
     // Setup ArgumentParser.
-    seqan::ArgumentParser parser("modify_string");
+    seqan::ArgumentParser parser("Print fasta stats");
 
-    addArgument(parser, seqan::ArgParseArgument(
-        seqan::ArgParseArgument::STRING, "TEXT"));
+    //addArgument(parser, seqan::ArgParseArgument(
+    //    seqan::ArgParseArgument::STRING, "TEXT"));
 
     addOption(parser, seqan::ArgParseOption(
-        "i", "period", "Period to use for the index.",
+        "m", "minlen", "Minimum length sequence to include in stats (default " + std::to_string(DEFAULT_MIN_LEN) + ")",
         seqan::ArgParseArgument::INTEGER, "INT"));
+
     addOption(parser, seqan::ArgParseOption(
-        "U", "uppercase", "Select to-uppercase as operation."));
+        "v", "version", "Display program version and exit"));
+
+    addOption(parser, seqan::ArgParseOption(
+        "b", "verbose", "Print more stuff about what's happening"));
 
     // Parse command line.
     seqan::ArgumentParser::ParseResult res = seqan::parse(parser, argc, argv);
@@ -24,7 +41,17 @@ int main(int argc, char const ** argv)
     if (res != seqan::ArgumentParser::PARSE_OK)
         return res == seqan::ArgumentParser::PARSE_ERROR;
 
+
+    BiotoolOptions options;
+
+    getOptionValue(options.minlen, parser, "minlen");
+    options.verbose = isSet(parser, "verbose");
+    options.version = isSet(parser, "version");
+    // getArgumentValue(options.text, parser, 0);
+
+
     // Extract option values and print them.
+    /*
     unsigned period = 0;
     getOptionValue(period, parser, "period");
     bool toUppercase = isSet(parser, "uppercase");
@@ -34,6 +61,7 @@ int main(int argc, char const ** argv)
     std::cout << "period   \t" << period << '\n'
               << "uppercase\t" << toUppercase << '\n'
               << "text     \t" << text << '\n';
+    */
 
     return 0;
 }

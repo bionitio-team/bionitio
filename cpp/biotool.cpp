e936cf7f8e04dcc91318ac0fe8da81f20026e2b3
Bernie Pope <bjpope@unimelb.edu.au>
2016-05-14 22:21:55 +1000

    Compute stats on reads and filter minlen

--- cpp/biotool.cpp
+++ cpp/biotool.cpp
@@ -1,67 +1,123 @@
 #include <iostream>
 #include <seqan/arg_parse.h>
+#include <seqan/seq_io.h>
 
-#define DEFAULT_MIN_LEN 0
+using namespace seqan;
+using namespace std;
+
+const char *PROGRAM_NAME = "biotool";
+const char *HEADER = "FILENAME\tNUMSEQ\tTOTAL\tMIN\tAVG\tMAX";
+const unsigned DEFAULT_MIN_LEN = 0;
+
+typedef enum {Error_command_line=1, Error_open_file=2, Error_parse_file=3} exit_status;
 
 struct BiotoolOptions
 {
     unsigned minlen;
     bool verbose;
     bool version;
-    //seqan::CharString text;
+    vector<string> fasta_files;
 
     BiotoolOptions() :
-    minlen(DEFAULT_MIN_LEN), verbose(false), version(false)
+    minlen(DEFAULT_MIN_LEN), verbose(false), version(false), fasta_files({})
     {}
 };
 
-int main(int argc, char const ** argv)
+void print_error(const char *message)
 {
-    // Setup ArgumentParser.
-    seqan::ArgumentParser parser("Print fasta stats");
+    cerr << PROGRAM_NAME << " ERROR: " << message << endl;
+}
 
-    //addArgument(parser, seqan::ArgParseArgument(
-    //    seqan::ArgParseArgument::STRING, "TEXT"));
-
-    addOption(parser, seqan::ArgParseOption(
-        "m", "minlen", "Minimum length sequence to include in stats (default " + std::to_string(DEFAULT_MIN_LEN) + ")",
-        seqan::ArgParseArgument::INTEGER, "INT"));
-
-    addOption(parser, seqan::ArgParseOption(
+BiotoolOptions parse_options(int argc, char const **argv)
+{
+    ArgumentParser parser(PROGRAM_NAME);
+    addArgument(parser, ArgParseArgument(
+        ArgParseArgument::STRING, "FASTA_FILE", true));
+    addOption(parser, ArgParseOption(
+        "m", "minlen", "Minimum length sequence to include in stats (default " + to_string(DEFAULT_MIN_LEN) + ")",
+        ArgParseArgument::INTEGER, "INT"));
+    addOption(parser, ArgParseOption(
         "v", "version", "Display program version and exit"));
-
-    addOption(parser, seqan::ArgParseOption(
+    addOption(parser, ArgParseOption(
         "b", "verbose", "Print more stuff about what's happening"));
+    ArgumentParser::ParseResult res = parse(parser, argc, argv);
 
-    // Parse command line.
-    seqan::ArgumentParser::ParseResult res = seqan::parse(parser, argc, argv);
-
-    // If parsing was not successful then exit with code 1 if there were errors.
-    // Otherwise, exit with code 0 (e.g. help was printed).
-    if (res != seqan::ArgumentParser::PARSE_OK)
-        return res == seqan::ArgumentParser::PARSE_ERROR;
-
+    if (res == ArgumentParser::PARSE_ERROR)
+    {
+        exit(Error_command_line);
+    }
 
     BiotoolOptions options;
-
     getOptionValue(options.minlen, parser, "minlen");
     options.verbose = isSet(parser, "verbose");
     options.version = isSet(parser, "version");
-    // getArgumentValue(options.text, parser, 0);
+    options.fasta_files = getArgumentValues(parser, 0);
+    return options;
+}
 
+void process_files(BiotoolOptions options)
+{
+    CharString id;
+    CharString seq;
 
-    // Extract option values and print them.
-    /*
-    unsigned period = 0;
-    getOptionValue(period, parser, "period");
-    bool toUppercase = isSet(parser, "uppercase");
-    seqan::CharString text;
-    getArgumentValue(text, parser, 0);
+    cout << HEADER << endl;
 
-    std::cout << "period   \t" << period << '\n'
-              << "uppercase\t" << toUppercase << '\n'
-              << "text     \t" << text << '\n';
-    */
+    for (string filename : options.fasta_files) {
+	SeqFileIn seqFileIn;
+
+        if (!open(seqFileIn, toCString(filename)))
+        {
+	    print_error("Could not open the file.");
+            exit(Error_open_file);
+        }
+
+        int max_len = 0;
+        int min_len = -1; 
+	int this_len;
+        unsigned total_len = 0; 
+        unsigned num_seqs = 0;
+	unsigned int average;
+
+        while(!atEnd(seqFileIn))
+        {
+	    try
+	    {
+	        readRecord(id, seq, seqFileIn);
+	    }
+            catch (Exception const & e)
+            {
+	        print_error(e.what());
+                exit(Error_parse_file);
+            }
+	    this_len = length(seq);
+	    if (this_len > options.minlen)
+            {
+	        num_seqs++;
+	        total_len += this_len;
+	        max_len = max(max_len, this_len);
+	        if (min_len < 0 || this_len < min_len)
+                {
+                    min_len = this_len;
+                }
+	    }
+        }
+	if (num_seqs > 0)
+        {
+            average = (unsigned int) floor((double) total_len / (double) num_seqs);
+	    cout << filename << '\t' << num_seqs << '\t' \
+		 << min_len << '\t' << average << '\t' << max_len << endl;
+        }
+	else
+        {
+	    cout << filename << "\t0\t0\t-\t-\t-" << endl;
+        }
+    }
+}
+
+int main(int argc, char const **argv)
+{
+    BiotoolOptions options = parse_options(argc, argv);
+    process_files(options);
 
     return 0;
 }

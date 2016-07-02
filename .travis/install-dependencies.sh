cacae85b513df2d1b842c1b92ba02b37c49c7c3a
Bernie Pope <bjpope@unimelb.edu.au>
2016-07-02 11:33:44 +1000

    Redirect some of the build output for CPP version to /dev/null to make travis CI log shorter and easier to read

--- .travis/install-dependencies.sh
+++ .travis/install-dependencies.sh
@@ -35,13 +35,13 @@ sudo curl -sSf https://static.rust-lang.org/rustup.sh | sh
 cd cpp
 BIOTOOL_CPP_PATH=`pwd`
 wget --no-check-certificate http://www.cmake.org/files/v3.4/cmake-3.4.1.tar.gz
-tar -xzf cmake-3.4.1.tar.gz 
+tar -xzf cmake-3.4.1.tar.gz > /dev/null 2&>1
 cd cmake-3.4.1/ 
-./configure
-sudo make install
+./configure > /dev/null 2&>1
+sudo make install > /dev/null 2&>1
 cd $BIOTOOL_CPP_PATH
 wget http://packages.seqan.de/seqan-library/seqan-library-2.1.1.tar.xz 
-tar xvf seqan-library-2.1.1.tar.xz
+tar xvf seqan-library-2.1.1.tar.xz > /dev/null 2&>1
 mkdir biotool-build
 cd biotool-build
 cmake $BIOTOOL_CPP_PATH -DCMAKE_MODULE_PATH=$BIOTOOL_CPP_PATH/seqan-library-2.1.1/share/cmake/Modules/ -DSEQAN_INCLUDE_PATH=$BIOTOOL_CPP_PATH/seqan-library-2.1.1/include/ -DCMAKE_CXX_FLAGS=-std=c++11

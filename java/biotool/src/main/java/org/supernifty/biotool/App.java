640a7281dbb74077c89dec92a86427f1abbfd7d4
Peter Georgeson <peter@supernifty.org>
2016-07-11 20:57:39 +1000

    add javadoc and lint cleanup

--- java/biotool/src/main/java/org/supernifty/biotool/App.java
+++ java/biotool/src/main/java/org/supernifty/biotool/App.java
@@ -1,66 +1,94 @@
+/**
+* Implementation of biotool functionality
+*
+* @author Peter Georgeson
+* @version 1.0
+*/
 package org.supernifty.biotool;
 
-import org.apache.commons.cli.CommandLine; 
-import org.apache.commons.cli.CommandLineParser; 
-import org.apache.commons.cli.DefaultParser; 
-import org.apache.commons.cli.OptionBuilder; 
-import org.apache.commons.cli.Options; 
-import org.apache.commons.cli.ParseException; 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 
 /**
- * Main entry point for application
+ * Main entry point for the biotool application.
+ * This class handles the command line interface for biotool,
+ * by parsing any command line arguments.
+ *
+ * The class also handles printing help and version information.
  */
-public class App 
-{
+public final class App {
+    /**
+     * private constructor prevents instantiation.
+     */
+    private FilePathHelper() {
+    }
 
+    /**
+     * write help message to stdout.
+     */
     private static void printHelp() {
         System.out.println(
-            "Synopsis:\n" +
-            "  Print fasta stats\n" +
-            "Usage:\n" +
-            "  biotool [options] contigs.fasta [another.fa ...]\n" +
-            "Options:\n" +
-            "  --help       Show this help\n" +
-            "  --version    Print version and exit\n" +
-            "  --verbose    Print more stuff about what's happening\n" +
-            "  --minlen N   Minimum length sequence to include in stats (default=0)"
+            "Synopsis:\n"
+            + "  Print fasta stats\n"
+            + "Usage:\n"
+            + "  biotool [options] contigs.fasta [another.fa ...]\n"
+            + "Options:\n"
+            + "  --help       Show this help\n"
+            + "  --version    Print version and exit\n"
+            + "  --verbose    Print more stuff about what's happening\n"
+            + "  --minlen N   Minimum length sequence to include in stats "
+            + "(default=0)"
         );
     }
 
+    /**
+     * write version information to stdout.
+     */
     private static void printVersion() {
-        System.out.println( "biotool version " + Package.getPackage("org.supernifty.biotool").getImplementationVersion() );
+        System.out.println("biotool version "
+            + Package.getPackage("org.supernifty.biotool").getImplementationVersion());
     }
 
-    public static void main( String[] args )
-    {
+    /**
+     * command line entry point.
+     * @param args command line arguments
+     */
+    public static void main(final String[] args) {
         // parse args
         Options options = new Options();
-        options.addOption( "h", "help", false, "Show this help" );
-        options.addOption( OptionBuilder.withLongOpt( "version" )
-                                .withDescription( "Print version and exit" )
-                                .create() );
-        options.addOption( "v", "verbose", false, "Print more stuff about what's happening" );
-        options.addOption( "m", "minlen", true, "Minimum length sequence to include in stats (default=0)" );
+        options.addOption("h", "help", false, "Show this help");
+        options.addOption(OptionBuilder.withLongOpt("version")
+                                .withDescription("Print version and exit")
+                                .create());
+        options.addOption("v", "verbose", false,
+            "Print more stuff about what's happening");
+        options.addOption("m", "minlen", true,
+            "Minimum length sequence to include in stats (default=0)");
 
         CommandLineParser parser = new DefaultParser();
         try {
-            CommandLine cmd = parser.parse( options, args );
-            if ( cmd.hasOption( "help" ) ) {
+            CommandLine cmd = parser.parse(options, args);
+            if (cmd.hasOption("help")) {
                 printHelp();
                 System.exit(0);
             }
-            if ( cmd.hasOption( "version" ) ) {
+            if (cmd.hasOption("version")) {
                 printVersion();
                 System.exit(0);
             }
             // minlen param
             int minlength = 0;
-            if ( cmd.hasOption( "minlen" ) ) {
+            if (cmd.hasOption("minlen")) {
                 try {
-                    minlength = Integer.parseInt(cmd.getOptionValue("minlen", "0"));
-                }
-                catch (NumberFormatException e) {
-                    System.err.println( "\n*** Invalid minlen option. Expected number, got '" + cmd.getOptionValue("minlen", "0") + "' ***\n" );
+                    minlength = Integer.parseInt(
+                        cmd.getOptionValue("minlen", "0"));
+                } catch (NumberFormatException e) {
+                    System.err.println("\n*** Invalid minlen option. Expected number, got '"
+                        + cmd.getOptionValue("minlen", "0") + "' ***\n");
                     printHelp();
                     System.exit(0);
                 }
@@ -68,28 +96,35 @@ public class App
             System.out.println("FILENAME\tTOTAL\tNUMSEQ\tMIN\tAVG\tMAX");
             if (cmd.getArgs().length == 0) {
                 try {
-                    FastaStats stats = new FastaStats(System.in, cmd.hasOption("verbose"), minlength);
-                    System.out.printf("%s\t%d\t%d\t%d\t%d\t%d\n", "stdin", stats.getTotal(), stats.getNumSeq(), stats.getMin(), stats.getAverage(), stats.getMax());
-                }
-                catch (java.io.IOException e) {
+                    FastaStats stats = new FastaStats(
+                        System.in, cmd.hasOption("verbose"), minlength);
+                    System.out.printf("%s\t%d\t%d\t%d\t%d\t%d\n", "stdin",
+                        stats.getTotal(),
+                        stats.getNumSeq(),
+                        stats.getMin(),
+                        stats.getAverage(),
+                        stats.getMax());
+                } catch (java.io.IOException e) {
                     System.err.println("Failed to read stdin: " + e.getMessage());
                 }
-            }
-            else {
+            } else {
                 // process files
                 for (String filename: cmd.getArgs()) {
                     try {
                         FastaStats stats = new FastaStats(filename, cmd.hasOption("verbose"), minlength);
-                        System.out.printf("%s\t%d\t%d\t%d\t%d\t%d\n", filename, stats.getTotal(), stats.getNumSeq(), stats.getMin(), stats.getAverage(), stats.getMax());
-                    }
-                    catch (java.io.IOException e) {
+                        System.out.printf("%s\t%d\t%d\t%d\t%d\t%d\n",
+                            filename,
+                            stats.getTotal(),
+                            stats.getNumSeq(),
+                            stats.getMin(),
+                            stats.getAverage(), stats.getMax());
+                    } catch (java.io.IOException e) {
                         System.err.println("Failed to open '" + filename + "': " + e.getMessage());
                     }
                 }
             }
-        }
-        catch (ParseException e) {
-            System.err.println( "\n*** Invalid command line arguments: " + e.getMessage() + " ***\n" );
+        } catch (ParseException e) {
+            System.err.println("\n*** Invalid command line arguments: " + e.getMessage() + " ***\n");
             printHelp();
         }
     }
--- java/biotool/src/main/java/org/supernifty/biotool/FastaStats.java
+++ java/biotool/src/main/java/org/supernifty/biotool/FastaStats.java
@@ -1,77 +1,137 @@
-
+/**
+* Implementation of biotool functionality
+*
+* @author Peter Georgeson
+* @version 1.0
+*/
 package org.supernifty.biotool;
 
-import java.io.File;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.FileInputStream;
 import java.util.LinkedHashMap;
 import org.biojava.nbio.core.sequence.ProteinSequence;
 import org.biojava.nbio.core.sequence.io.FastaReaderHelper;
 
+/**
+ * Calculates statistics for a FastaFile, either from an input stream or file.
+ */
 public class FastaStats {
-    public FastaStats(InputStream input, boolean verbose, int minlength) throws java.io.IOException {
+    /**
+     * Construct a FastaStats object from an input stream.
+     * @param input the input stream to read fasta sequences from
+     * @param verbose write additional details of progress to stderr
+     * @param minlength include stats for sequences that are at least this long
+     * @throws java.io.IOException if an error occurs while reading the input
+     */
+    public FastaStats(final InputStream input,
+        final boolean verbose,
+        final int minlength)
+        throws java.io.IOException {
         log("Reading from stdin...", verbose);
-        process( input, verbose, minlength );
+        process(input, verbose, minlength);
         log("Reading from stdin: done", verbose);
     }
 
-    public FastaStats(String filename, boolean verbose, int minlength) throws java.io.IOException {
+    /**
+     * Construct a FastaStats object from an input stream.
+     * @param filename filename to read fasta sequences from
+     * @param verbose write additional details of progress to stderr
+     * @param minlength include stats for sequences that are at least this long
+     * @throws java.io.IOException if an error occurs while reading the file
+     */
+    public FastaStats(final String filename,
+        final boolean verbose,
+        final int minlength)
+        throws java.io.IOException {
         log("Reading " + filename + "...", verbose);
-        process( new FileInputStream(filename), verbose, minlength );
+        process(new FileInputStream(filename), verbose, minlength);
         log("Reading " + filename + ": done", verbose);
     }
 
-    private void process(InputStream input, boolean verbose, int minlength) throws java.io.IOException {
-        LinkedHashMap<String, ProteinSequence> result = FastaReaderHelper.readFastaProteinSequence(input);
+    /**
+     * Calculate stats for a set of fasta sequences read from an input stream.
+     * @param input input stream to read fasta sequences from
+     * @param verbose write additional details of progress to stderr
+     * @param minlength include stats for sequences that are at least this long
+     * @throws java.io.IOException if an error occurs while reading the input
+     */
+    private void process(final InputStream input,
+        final boolean verbose,
+        final int minlength)
+        throws java.io.IOException {
+        LinkedHashMap<String, ProteinSequence> result =
+            FastaReaderHelper.readFastaProteinSequence(input);
         total = 0;
         min = Integer.MAX_VALUE;
         max = 0;
         numseq = 0;
         for (ProteinSequence sequence: result.values()) {
-            int sequence_size = sequence.getLength();
-            if (sequence_size >= minlength) {
+            int sequenceSize = sequence.getLength();
+            if (sequenceSize >= minlength) {
                 total += 1;
-                numseq += sequence_size;
-                min = Math.min(min, sequence_size);
-                max = Math.max(max, sequence_size);
+                numseq += sequenceSize;
+                min = Math.min(min, sequenceSize);
+                max = Math.max(max, sequenceSize);
             }
         }
     }
 
-    public int getTotal() {
+    /**
+     * @return total number of bases found
+     */
+    public final int getTotal() {
         return total;
     }
 
-    public int getNumSeq() {
+    /**
+     * @return number of sequences found
+     */
+    public final int getNumSeq() {
         return numseq;
     }
 
-    public int getMin() {
+    /**
+     * @return length of shortest sequence
+     */
+    public final int getMin() {
         return min;
     }
 
-    public int getAverage() {
+    /**
+     * @return average length of sequences (or 0 if no sequences found)
+     */
+    public final int getAverage() {
         if (total > 0) {
             return numseq / total;
-        }
-        else {
+        } else {
             return 0;
         }
     }
 
-    public int getMax() {
+    /**
+     * @return length of longest sequence
+     */
+    public final int getMax() {
         return max;
     }
 
-    private void log(String msg, boolean verbose) {
+    /**
+     * log a message to stderr if verbose is set.
+     * @param msg message to log
+     * @param verbose whether to actually log the message
+     */
+    private void log(final String msg, final boolean verbose) {
         if (verbose) {
             System.err.println(msg);
         }
     }
 
+    /** total number of bases observed. */
     private int total;
+    /** number of sequences seen. */
     private int numseq;
+    /** length of shortest sequence. */
     private int min;
+    /** length of longest sequence. */
     private int max;
 }

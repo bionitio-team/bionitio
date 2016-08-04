8c89257b9b7b58dbdeb977295e95863793877b70
Bernie Pope <bjpope@unimelb.edu.au>
2016-08-04 21:00:20 +1000

    Correct c++ exit status values

--- cpp/exit_status.h
+++ cpp/exit_status.h
@@ -1,4 +1,4 @@
 #ifndef BIOTOOL_EXIT_STATUS_H
 #define BIOTOOL_EXIT_STATUS_H
-typedef enum {Success=0, Error_command_line=1, Error_open_file=2, Error_parse_file=3} exit_status;
+typedef enum {Success=0, Error_open_file=1, Error_command_line=2, Error_parse_file=3} exit_status;
 #endif

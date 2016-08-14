13a2e7ab3d00511257d56b2bfd7aa47212d55069
Bernie Pope <bjpope@unimelb.edu.au>
2016-08-14 16:52:09 +1000

    Add comments to Haskell implementation

--- haskell/src/Main.hs
+++ haskell/src/Main.hs
@@ -1,72 +1,144 @@
 {-# LANGUAGE RecordWildCards #-}
+
+{-|
+Module      : Main 
+Description : The main entry point for the program.
+Copyright   : (c) Bernie Pope, 2016 
+License     : MIT 
+Maintainer  : bjpope@unimelb.edu.au
+Stability   : experimental
+Portability : POSIX
+
+This module orchestrates the overall behaviour of the program. The main
+parts are:
+ * Parse command line arguments. 
+ * Process each FASTA file in sequence.
+ * Pretty print output. 
+-}
+
 module Main where
 
-import Stats (sequenceStats, Stats(..), average)
+import Stats
+   (sequenceStats, Stats(..), average)
 import Bio.Sequence.Fasta
-   ( readFasta, hReadFasta, seqlength, Sequence )
-import System.IO ( stdin )
+   (readFasta, hReadFasta)
+import System.IO (stdin)
 import Options.Applicative 
-   ( Parser, option, auto, long, metavar, help, value
-   , many, argument, str, info, execParser, switch
-   , fullDesc, (<*>), (<$>), (<>), helper, progDesc )
+   (Parser, option, auto, long, short, metavar, help, value,
+   many, argument, str, info, execParser, switch,
+   fullDesc, (<*>), (<$>), (<>), helper, progDesc)
 import Data.List
-   ( foldl', intersperse )
+   (intersperse)
 
+-- | Default value for the --minlen command line argument. Setting it to
+-- zero means that, by default, no sequences will be skipped.
 defaultMinLengthThreshold :: Integer
 defaultMinLengthThreshold = 0
 
+-- | Header row for the output.
+header :: String
+header = "FILENAME\tNUMSEQ\tTOTAL\tMIN\tAVG\tMAX"
+
+-- | Brief description of the program for command line help/usage message.
+programDescription
+   = "Read FASTA file(s), compute and print basic statistics for each one"
+
+-- | Command line argument options.
 data Options = Options
-   { minLengthThreshold :: Integer
+   { -- | Minimum length sequence considered by the program. Sequences
+     -- shorter than this length are ignored.
+     minLengthThreshold :: Integer
+     -- | If True, print the version of the program and exit.
    , version :: Bool
+     -- | If True, make the program produce more detailed output
+     -- about its progress.
    , verbose :: Bool
+     -- | A possibly empty list of FASTA file paths to be processed
+     -- by the program. 
    , fastaFiles :: [FilePath]
    }
    deriving (Eq, Ord, Show)
 
+-- | Specification of the command line options for the program:
+--  * --minlen N, N is a non-negative integer 
+--  * --version
+--  * --verbose 
+--  * list of FASTA filename paths
 defineOptions :: Parser Options
 defineOptions = Options
    <$> option auto 
-         (long "minlen"
-            <> metavar "N"
-            <> help ("Minimum length sequence to include in stats (default=" ++ show defaultMinLengthThreshold ++ ")")
-            <> value defaultMinLengthThreshold)
-   <*> switch (long "version" <> help "Print version and exit")
-   <*> switch (long "verbose" <> help "Print more stuff about what's happening")
+          (long "minlen"
+           <> short 'm'
+           <> metavar "N"
+           <> help ("Minimum length sequence to include in stats (default="
+                 ++ show defaultMinLengthThreshold ++ ")")
+           <> value defaultMinLengthThreshold)
+   <*> switch
+          (long "version"
+           <> short 'v'
+           <> help "Print version and exit")
+   <*> switch
+          (long "verbose"
+           <> help "Print more stuff about what's happening")
+   -- Allow zero of more FASTA files to be specified as positional arguments.
    <*> many (argument str (metavar "[FASTA_FILE ...]"))
 
-main :: IO ()
-main = do
-   options <- execParser optionParser 
-   processFastaFiles options $ fastaFiles options
-   where
-   optionParser =
-      info (helper <*> defineOptions)
-           (fullDesc <> progDesc "Print fasta stats")
-
-header :: String
-header = "FILENAME\tNUMSEQ\tTOTAL\tMIN\tAVG\tMAX"
-
-processFastaFiles :: Options -> [FilePath] -> IO ()
+-- | Compute statistics for each input FASTA file specified on the
+-- command line. If no files were specified then process the
+-- stdin as a single FASTA file. 
+processFastaFiles :: Options    -- ^ Command line options
+                  -> [FilePath] -- ^ Possibly empty list of FASTA file paths
+                  -> IO ()
+-- No files specified on command line, so read from stdin
+-- XXX reduce code duplication here
 processFastaFiles options [] = do
    sequences <- hReadFasta stdin
-   putStrLn header
    putStrLn $ prettyOutput "stdin" $ 
       sequenceStats (minLengthThreshold options) sequences 
+-- One or more files specified on command line, process
+-- each one in sequence.
 processFastaFiles options files@(_:_) =
-   putStrLn header >> mapM_ (processFile options) files
+   mapM_ (processFile options) files
 
-processFile :: Options -> FilePath -> IO ()
+-- | Compute statistics for a single FASTA file
+processFile :: Options  -- ^ Command line options
+            -> FilePath -- ^ File path of FASTA file
+            -> IO ()
 processFile options filePath = do
    sequences <- readFasta filePath
    putStrLn $ prettyOutput filePath $
       sequenceStats (minLengthThreshold options) sequences 
 
-prettyOutput :: FilePath -> Maybe Stats -> String
-prettyOutput filePath Nothing =
-   filePath ++ "\t0\t0\t-\t-\t-"
-prettyOutput filePath (Just stats@(Stats {..})) =
-   concat $ intersperse "\t" (filePath : numbers)
+-- | Render program outout as a String. If no statistics are available it means
+-- that the input file was empty, so no min, max or average is available. In
+-- that case we just show zeros for num sequences and num bases, and dashes for the
+-- remaining values.
+prettyOutput :: String      -- ^ A label for the input file. Normally the file name.
+             -> Maybe Stats -- ^ Computed statistics about the file.
+                            -- None if file was empty.
+             -> String      -- ^ Rendered output, tab separated.
+prettyOutput label Nothing =
+   label ++ "\t0\t0\t-\t-\t-"
+prettyOutput label (Just stats@(Stats {..})) =
+   concat $ intersperse "\t" (label : numbers)
    where
+   -- If we cannot compute an average, show a dash.
    averageStr = maybe "-" show (average stats)
    numbers = [ show numSequences, show numBases, show minSequenceLength
              , averageStr, show maxSequenceLength ]
+
+
+-- | The entry point for the program.
+--  * Parse the command line arguments.
+--  * Print the output header.
+--  * Process each input FASTA file, compute stats and display
+--    results.
+main :: IO ()
+main = do
+   options <- execParser optionParser 
+   putStrLn header
+   processFastaFiles options $ fastaFiles options
+   where
+   optionParser =
+      info (helper <*> defineOptions)
+           (fullDesc <> progDesc programDescription)
--- haskell/src/Stats.hs
+++ haskell/src/Stats.hs
@@ -1,37 +1,91 @@
 {-# LANGUAGE RecordWildCards #-}
+
+{-|
+Module      : Stats 
+Description : Compute various simple statistics on a FASTA file. 
+Copyright   : (c) Bernie Pope, 2016 
+License     : MIT 
+Maintainer  : bjpope@unimelb.edu.au
+Stability   : experimental
+Portability : POSIX
+
+Read a single FASTA file as input and compute:
+ * Num sequences.
+ * Total number of bases in all sequences.
+ * Minimum sequence length.
+ * Maximum sequence length.
+ * Average sequence length, as an integer , rounded towards zero.
+
+Sequences whose length is less than a specified minimum length are
+ignored (skipped) and not included in the calculation of the statistics.
+
+The basic statistics cannot be computed for empty files, and those
+with no reads that meet the mimimum length requirement. In such cases
+the result is Nothing.
+
+We do not make any assumptions about the type of data stored in the
+FASTA file, it could be DNA, proteins or anything else. No checks are
+made to ensure that the FASTA file makes sense biologically.
+
+Whitespace within the sequences will be ignored.
+-}
+
 module Stats (sequenceStats, Stats(..), average) where
 
 import Bio.Sequence.Fasta
-   ( seqlength, Sequence )
+   (seqlength, Sequence)
 import Data.List
-   ( foldl', intersperse )
+   (foldl')
 
+-- | Basic statistics computed for a FASTA file. Note: average is
+-- not included, but can be computed from the 'average' function.
+-- Note that all values are with respect to only those reads whose
+-- length is at least as long as the minimum.
 data Stats =
    Stats 
-   { numSequences :: !Integer
+   { -- | Total number of sequences in the file. 
+     numSequences :: !Integer      
+     -- | Total number of bases from all the sequences in the file. 
    , numBases :: !Integer
+     -- | Minimum length of all sequences in the file.
    , minSequenceLength :: !Integer
+     -- | Maximum length of all sequences in the file.
    , maxSequenceLength :: !Integer
    }
    deriving (Eq, Ord, Show)
 
+-- | Average length of all sequences in a FASTA file.
+-- Returns Nothing if the number of counted sequences is zero.
+-- Average is rounded to an integer towards zero.
 average :: Stats -> Maybe Integer
 average (Stats {..}) 
    | numSequences > 0 =
         Just $ floor (fromIntegral numBases / fromIntegral numSequences)
    | otherwise = Nothing
 
-sequenceStats :: Integer -> [Sequence] -> Maybe Stats
+-- | Compute basic statistics for a list of sequences taken from
+-- a FASTA file. Sequences whose length is less than the specified minimum
+-- are ignored.
+sequenceStats :: Integer     -- ^ Minimum sequence length. Sequences shorter
+                             -- than this are ignored (skipped).
+              -> [Sequence]  -- ^ A list of all the sequences from a FASTA file.
+              -> Maybe Stats -- ^ Basic statistics for all the sequences. Is Nothing
+                             -- if the list of sequences does not contain any
+                             -- elements which are at least as long as the minimum.
 sequenceStats minLength sequences =
    case filteredLengths of
       [] -> Nothing
       first:rest -> 
         Just $ foldl' updateStats (initStats first) rest 
    where
+   -- Collect all sequences whose length is at least as long as the
+   -- minumum. Lazy evaluation means that this can be done in a streaming
+   -- fashion, so we don't have to read the entire file at once.
    filteredLengths =
       filter (\x -> sequenceLengthInteger x >= minLength) sequences
 
-initStats :: Sequence -> Stats
+-- | Initial value for statistics, given the first sequence.
+initStats :: Sequence -> Stats    
 initStats sequence = Stats
    { numSequences = 1
    , numBases = thisLength 
@@ -41,7 +95,10 @@ initStats sequence = Stats
    where
    thisLength = sequenceLengthInteger sequence
 
-updateStats :: Stats -> Sequence -> Stats
+-- | Update the stats given the next sequence from the FASTA file.
+updateStats :: Stats    -- ^ Current value of stats.
+            -> Sequence -- ^ Next sequence from the FASTA file.
+            -> Stats    -- ^ New, updated stats.
 updateStats oldStats@(Stats {..}) sequence =
    Stats newNumSequences newNumBases
          newMinSequenceLength newMaxSequenceLength 

12b3c70717b7df1a54734f81d482417a92a970c6
Bernie Pope <bjpope@unimelb.edu.au>
2016-08-14 20:22:21 +1000

    Correct error messages in Python test cases, remove redirection of output to /dev/null

--- .travis/test-python.sh
+++ .travis/test-python.sh
@@ -3,20 +3,15 @@
 set -e
 errors=0
 
-#TOP_DIR=`pwd`
-#cd python
-
 # Run unit tests
-python python/biotool/biotool_test.py > /dev/null 2>&1 || {
-    echo "'python biotool_test.py' failed"
+python python/biotool/biotool_test.py || {
+    echo "'python python/biotool/biotool_test.py' failed"
     let errors+=1
 }
 
-#cd $TOP_DIR
 # Check program style
-# pylint -E python/biotool > /dev/null 2>&1 || {
 pylint -E python/biotool || {
-    echo "'pylint -E biotool' failed"
+    echo "'pylint -E python/biotool' failed"
     let errors+=1
 }
 

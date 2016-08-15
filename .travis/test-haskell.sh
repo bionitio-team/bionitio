2f7ca082922a9cc7e1e73a0971f2cc3d61cdcce3
Bernie Pope <bjpope@unimelb.edu.au>
2016-08-15 22:29:26 +1000

    Add hlint static analysis to Haskell test case for travis-ci

--- .travis/test-haskell.sh
+++ .travis/test-haskell.sh
@@ -12,6 +12,12 @@ stack test > /dev/null 2>&1 || {
     let errors+=1
 }
 
+# Run hlint static analysis 
+~/.local/bin/hlint src || {
+    echo "'~/.local/bin/hlint src' failed"
+    let errors+=1
+}
+
 [ "$errors" -gt 0 ] && {
     echo "There were $errors errors found"
     exit 1

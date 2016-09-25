c385744701984308574bc1e5878468d631bb2d85
David Powell <david@drp.id.au>
2016-09-25 12:44:22 +0100

    Make ruby work with defined exit codes

--- ruby/biotool.rb
+++ ruby/biotool.rb
@@ -9,7 +9,7 @@ require 'bio'
 opts = { :minlen => 0,
          :verbose => 0,
        }
-OptionParser.new do |o|
+o = OptionParser.new do |o|
     exe = File.basename($0)
     o.banner = "Usage:\n  #{exe} [options] contigs.fasta [another.fa ...]"
     o.separator ""
@@ -20,10 +20,23 @@ OptionParser.new do |o|
 
     o.on('--minlen N', 'Minimum length sequence to include in stats') { |m| opts[:minlen] = m.to_i}
     o.on('--verbose', "Print more stuff about what's happening") { |v| opts[:verbose] += 1 }
-    o.parse!
 end
 
+# Parse command line, and exit on a bad option
+begin
+    o.parse! ARGV
+rescue OptionParser::InvalidOption => e
+    $stderr.puts e
+    $stderr.puts o
+    exit 2   # Exit code 2 on invalid option
+end
+
+# Class to parse, filter and format stats on a fasta file
 class FastaSummary
+    # Parse and filter the given fasta file.
+    # Valid opts are:
+    #   :minlen -> sequences strictly less than this are ignored
+    #   :verbose -> enable logging
     def initialize(file, opts)
       @file=file
       @opts = opts
@@ -31,7 +44,7 @@ class FastaSummary
       @n=0
       @bp=0
       fasta = Bio::FastaFormat.open(@file)
-      fasta.each do |seq| 
+      fasta.each do |seq|
           process(seq)
       end
     end
@@ -40,12 +53,14 @@ class FastaSummary
         %w(FILENAME TOTAL NUMSEQ MIN AVG MAX).join("\t")
     end
 
+    # String format for stats on this fasta file.  Return nil if no sequences found
     def pretty
         return nil if @n==0
         [@file, @n, @bp, @min, @bp/@n, @max].join("\t")
     end
 
   private
+    # filter each sequence in the fasta file and collect stats
     def process(seq)
         return if seq.length < @opts[:minlen]
         $stderr.puts [@file, seq.entry_id, seq.length ].join("\t") if @opts[:verbose] >= 2
@@ -58,16 +73,21 @@ class FastaSummary
 end
 
 
+# Default to stdin if not files given
 files = ARGV.length==0 ? ["/dev/stdin"] : ARGV
 
 puts FastaSummary.hdr
+# Process each fasta file
 files.each do |file|
     $stderr.puts "Processing: #{file}" if opts[:verbose]>=1
-    summary = FastaSummary.new(file, opts).pretty
-    if summary
-        puts summary
-    else
-        $stderr.puts "Skipping #{file} - doesn't seem to be FASTA?";
+    begin
+        summary = FastaSummary.new(file, opts).pretty
+        if summary
+            puts summary
+        else
+            $stderr.puts "Skipping #{file} - doesn't seem to be FASTA?";
+        end
+    rescue Errno::ENOENT
+        exit 1            # Exit code 1 on missing file
     end
 end
-

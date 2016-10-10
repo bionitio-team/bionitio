16738d010e786e1f0ea0f369b2a47ca8433282f9
Bernie Pope <bjpope@unimelb.edu.au>
2016-10-10 20:32:11 +1100

    Partial documentation of CPP code

--- cpp/src/biotool.cpp
+++ cpp/src/biotool.cpp
@@ -1,3 +1,19 @@
+/*
+File:       : biotool.cpp 
+Description : The main entry point for the program.
+Copyright   : (c) Bernie Pope, 2016 
+License     : MIT 
+Maintainer  : bjpope@unimelb.edu.au
+Stability   : stable 
+Portability : POSIX
+
+This module orchestrates the overall behaviour of the program. The main
+parts are:
+ * Parse command line arguments. 
+ * Process each FASTA file in sequence.
+ * Pretty print output. 
+*/    
+
 #include <iostream>
 #include <seqan/seq_io.h>
 #include "options.h"
@@ -9,19 +25,35 @@
 using namespace seqan;
 using namespace std;
 
+/*
+    Process each input FASTA file.
+        - print the output header
+        - check if any FASTA files were given on the command line
+        - if zero FASTA files, read from stdin
+
+    arguments:
+        options: the command line options of the program
+    result:
+        void
+*/
 void process_files(Options options)
 {
     cout << HEADER << endl;
     FastaStats fasta_stats;
 
+    // Check how many files were specified on the command line.
     if (options.fasta_files.size() == 0)
     {
+        // Try to read a FASTA file from stdin
         SeqFileIn seq_file(cin);
+        // Compute various statistics for the file
         fasta_stats.from_file(seq_file, options.minlen);
+        // Pretty print the results
         cout << fasta_stats.pretty("stdin") << endl;
     }
     else
     {
+        // Process each file specified on the command line
         for (string filename : options.fasta_files)
         {
             SeqFileIn seq_file;
@@ -29,12 +61,23 @@ void process_files(Options options)
             {
                 exit_with_error("Could not open the file.", Error_open_file);
             }
+            // Compute various statistics for the file
             fasta_stats.from_file(seq_file, options.minlen);
+            // Pretty print the results
             cout << fasta_stats.pretty(filename) << endl;
         }
     }
 }
 
+/*
+    Entry point for the program
+        - parse command line options
+        - process input files
+
+    arguments:
+        argc: the number of strings in the argument vector
+        argv: the argument vector, an array of strings
+*/
 int main(int argc, const char **argv)
 {
     Options options(argc, argv);
--- cpp/src/error.cpp
+++ cpp/src/error.cpp
@@ -1,3 +1,13 @@
+/*
+File:       : error.cpp 
+Description : Utilities for error reporting 
+Copyright   : (c) Bernie Pope, 2016 
+License     : MIT 
+Maintainer  : bjpope@unimelb.edu.au
+Stability   : stable 
+Portability : POSIX
+*/
+
 #include <cstdlib>
 #include <iostream>
 #include "error.h"
@@ -6,12 +16,37 @@
 
 using namespace std;
 
+/*
+    Print an error message to stderr and exit the program.
+
+    arguments:
+        - message: the error message string, the program
+              name and "ERROR" will be prepended onto this
+              message. The message will be printed to
+              stderr.
+        - exit_status: the exit status to return when the
+              program exits
+
+    result:
+        void
+*/
 void exit_with_error(string message, exit_status status)
 {
     print_error(message);
     exit(status);
 }
 
+/*
+    Print an error message to stderr.
+
+    arguments:
+        - message: the error message string, the program
+              name and "ERROR" will be prepended onto this
+              message. The message will be printed to
+              stderr.
+    result:
+        void
+*/
 void print_error(string message)
 {
     cerr << PROGRAM_NAME << " ERROR: " << message << endl;

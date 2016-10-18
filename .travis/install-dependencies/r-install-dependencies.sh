647760d7de4829fe02db96e0c3c4cc3be9fe0582
Jessica Chung <jchung@unimelb.edu.au>
2016-10-18 13:37:35 +1100

    R: Change argparse library to allow for defined exit status

--- .travis/install-dependencies/r-install-dependencies.sh
+++ .travis/install-dependencies/r-install-dependencies.sh
@@ -7,6 +7,6 @@ echo 'R install'
    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys E084DAB9
    echo 'deb http://cran.rstudio.com/bin/linux/ubuntu trusty/' | sudo tee -a /etc/apt/sources.list
    sudo apt-get update && sudo apt-get install r-base r-base-dev -y
-   sudo Rscript -e "install.packages('argparse', repos='http://cran.rstudio.org')"
+   sudo Rscript -e "install.packages('optparse', repos='http://cran.rstudio.org')"
    sudo Rscript -e "install.packages('seqinr', repos='http://cran.rstudio.org')"
 ) > /dev/null 2>&1

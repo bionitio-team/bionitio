6e8e69835b9b850f80725ce670f4274d4b295587
Bernie Pope <bjpope@unimelb.edu.au>
2017-01-15 23:55:31 +1100

    Add command line argumnets to Clojure version, allow input from stdin, filter minlen length sequences

--- clojure/project.clj
+++ clojure/project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [clj-biosequence "0.5.2"]]
+                 [clj-biosequence "0.5.2"]
+                 [org.clojure/tools.cli "0.3.5"]]
   :main ^:skip-aot biotool.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- clojure/src/biotool/core.clj
+++ clojure/src/biotool/core.clj
@@ -1,60 +1,120 @@
 (ns biotool.core
-  (:gen-class)
-  (:require [clj-biosequence.core :as bs]))
+  (:require [clj-biosequence.core :as bs]
+            [clojure.string :as string]
+            [clojure.tools.cli :refer [parse-opts]])
+  (:gen-class))
 
-(def initial_stats
+(def initial-stats
   "Initial value of stats, before any sequences have been considered"
-  {:num_sequences 0
-   :num_bases 0
-   :min_sequence_length nil
-   :max_sequence_length nil})
+  {:num-sequences 0
+   :num-bases 0
+   :min-sequence-length nil
+   :max-sequence-length nil})
 
-(defn update_stats
+(defn update-stats
   "Update stats accumulator with information from the next sequence"
   [stats record]
-  (let [{:keys [num_sequences num_bases min_sequence_length max_sequence_length]} stats
+  (let [{:keys [num-sequences num-bases min-sequence-length max-sequence-length]} stats
         sequence (:sequence record)
-        sequence_length (count sequence)]
-       {:num_sequences (inc num_sequences) 
-        :num_bases (+ num_bases sequence_length)
-        :min_sequence_length (if min_sequence_length
-                                 (min min_sequence_length sequence_length)
-                                 sequence_length)
-        :max_sequence_length (if max_sequence_length
-                                 (max max_sequence_length sequence_length)
-                                 sequence_length)}))
+        sequence-length (count sequence)]
+       {:num-sequences (inc num-sequences) 
+        :num-bases (+ num-bases sequence-length)
+        :min-sequence-length (if min-sequence-length
+                                 (min min-sequence-length sequence-length)
+                                 sequence-length)
+        :max-sequence-length (if max-sequence-length
+                                 (max max-sequence-length sequence-length)
+                                 sequence-length)}))
+
+(defn print-results
+  "Display the computed stats in pretty fashion on the standard output"
+  [filename stats]
+  (let [{:keys [num-sequences num-bases min-sequence-length max-sequence-length]} stats
+        average (if (> num-sequences 0)
+                    (int (Math/floor (double (/ num-bases num-sequences))))
+                    nil)
+        average-str (if average (str average) "-")
+        num-sequences-str (str num-sequences)
+        num-bases-str (str num-bases)
+        min-sequence-length-str
+          (if min-sequence-length (str min-sequence-length) "-")
+        max-sequence-length-str
+          (if max-sequence-length (str max-sequence-length) "-")
+        output-str (str filename "\t"
+                        num-sequences-str "\t"
+                        num-bases-str "\t"
+                        min-sequence-length-str "\t"
+                        average-str "\t"
+                        max-sequence-length-str)]
+  (println output-str)))
+
+(defn length-gte-minlen?
+  [minlen record]
+  (let [sequence (:sequence record)
+        num-bases (count sequence)]
+    (>= num-bases minlen)))
+
+(defn process-fasta-reader
+  [minlen reader filename]
+    (print-results filename
+      (reduce
+        update-stats
+        initial-stats 
+        (filter
+          (fn [record] (length-gte-minlen? minlen record))
+          (bs/biosequence-seq reader)))))
+
+(defn process-fasta-file 
+  [minlen file]
+  (with-open [reader (bs/bs-reader (bs/init-fasta-file file :iupacAminoAcids))]
+    (process-fasta-reader minlen reader file)))
+
+(defn process-stdin
+  [minlen]
+  (with-open [reader (bs/init-fasta-reader (java.io.BufferedReader. *in*) :iupacAminoAcids)]
+    (process-fasta-reader minlen reader "stdin")))
 
 (def header "FILENAME\tTOTAL\tNUMSEQ\tMIN\tAVG\tMAX")
 
-(defn print_results
-  "Display the computed stats in pretty fashion on the standard output"
-  [filename stats]
+(defn process-fasta-files
+  [minlen files]
   (println header)
-  (let [{:keys [num_sequences num_bases min_sequence_length max_sequence_length]} stats
-        average (if (> num_sequences 0)
-                    (double (/ num_bases num_sequences))
-                    nil)
-        average_str (if average (str average) "-")
-        num_sequences_str (str num_sequences)
-        num_bases_str (str num_bases)
-        min_sequence_length_str
-          (if min_sequence_length (str min_sequence_length) "-")
-        max_sequence_length_str
-          (if max_sequence_length (str max_sequence_length) "-")
-        output_str (str filename "\t"
-                        num_sequences_str "\t"
-                        num_bases_str "\t"
-                        min_sequence_length_str "\t"
-                        average_str "\t"
-                        max_sequence_length_str)]
-  (println output_str)))
+  (if (empty? files)
+      (process-stdin minlen)
+      (doseq [file files]
+         (process-fasta-file minlen file))))
+
+(defn usage [options-summary]
+  (->> ["Print fasta stats"
+        ""
+        "Usage: program-name [options] FILES"
+        ""
+        "Options:"
+        options-summary]
+       (string/join \newline)))
+
+(defn error-msg [errors]
+  (str "The following errors occurred while parsing your command:\n\n"
+       (string/join \newline errors)))
+
+(defn exit [status msg]
+  (println msg)
+  (System/exit status))
+
+(def default-minlen 0)
+
+(def cli-options
+  [[nil "--minlen N" "Minimum length sequence to include in stats"
+    :default default-minlen :parse-fn #(Integer/parseInt %)]
+   [nil "--log LOG-FILE" "record program progress in LOG-FILE"]
+   ["-v" "--version"]
+   ["-h" "--help"]])
 
 (defn -main
   "Orchestrate the computation" 
-  [file]
-  (with-open [reader (bs/bs-reader (bs/init-fasta-file file :iupacAminoAcids))]
-    (print_results file
-      (reduce
-        update_stats
-        initial_stats 
-        (bs/biosequence-seq reader)))))
+  [& args]
+  (let [{:keys [options arguments errors summary]} (parse-opts args cli-options)]
+    (cond
+      (:help options) (exit 0 (usage summary))
+      errors (exit 1 (error-msg errors)))
+    (process-fasta-files (:minlen options) arguments)))

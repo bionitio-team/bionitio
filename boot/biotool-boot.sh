7ded8996bb9e1943561d84da1900dbadb2e1b6b8
Bernie Pope <bjpope@unimelb.edu.au>
2017-01-22 16:14:32 +1100

    Optional licenses to use with biotool-derived projects

--- boot/biotool-boot.sh
+++ boot/biotool-boot.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
 # 1. Parse command line arguments.
-# 2. Try to create new directory for the project.
-# 3. Clone biotool git repository into the newly created directory.
-# 4. Recursively copy source tree from the git repository into the project directory.
-# 5. Recursively copy test_data directory from the git repository into the project directory.
-# 6. Remove the cloned git repository.
-# 7. Rename biotool to the new project name.
-# 8. Create repository for new project.
+# 2. Check dependencies.
+# 3. Try to create new directory for the project.
+# 4. Clone biotool git repository into the newly created directory.
+# 5. Recursively copy source tree from the git repository into the project directory.
+# 6. Recursively copy test_data directory from the git repository into the project directory.
+# 7. Set the license for the project.
+# 8. Remove the cloned git repository.
+# 9. Rename biotool to the new project name.
+# 10. Create repository for new project.
 
 #set -x
 
@@ -15,6 +17,8 @@ program_name="biotool-boot.sh"
 # The name of the programming language that the user wants to use from the
 # available biotool implementations
 language=""
+# The license used for the new project. It defaults to MIT.
+license="MIT"
 # The name of the new software project that the user wants to create
 new_project_name=""
 # Verbose output
@@ -29,23 +33,35 @@ cat << UsageMessage
 ${program_name}: initialise a new bioinformatics project, starting from biotool
 
 Usage:
-    ${program_name} [-h] [-v] -l language -n new_project_name
+    ${program_name} [-h] [-v] [-c license] -l language -n new_project_name
 
 Example:
-    ${program_name} -l python -n skynet
+    ${program_name} -c BSD-3-Clause -l python -n skynet
 
 The above example Will try to initialise a new project in directory 'skynet'
-based on the 'python' biotool implementation.
+based on the 'python' biotool implementation, using the BSD-3-Clause license.
 
 If a directory already exists in the current working directory with the same
 name as the new_project_name then this installer will not continue.
 
+If no license is supplied, it will default to using the MIT license.
+
 Valid languages are:
 c, clojure, cpp, haskell, java, js, perl5, python, r, ruby, rust
+
+Valid licenses are:
+Apache-2.0, BSD-2-Clause, BSD-3-Clause, GPL-2.0, GPL-3.0, MIT
+
+These are just a selection of commonly used licenses, but you are free to
+choose another, if you so desire.
 
 -h shows this help message
 
 -v verbose output
+
+Dependencies:
+
+   - git must be installed on your computer to use this script.
 
 UsageMessage
 }
@@ -54,13 +70,15 @@ UsageMessage
 function parse_args {
     local OPTIND opt
 
-    while getopts "hl:n:v" opt; do
+    while getopts "hc:l:n:v" opt; do
         case "${opt}" in
             h)
                 show_help
                 exit 0
                 ;;
             l)  language="${OPTARG}"
+                ;;
+            c)  license="${OPTARG}"
                 ;;
             n)  new_project_name="${OPTARG}"
                 ;;
@@ -87,10 +105,27 @@ function parse_args {
             exit 2
     esac
 
+    case ${license} in
+	Apache-2.0|BSD-2-Clause|BSD-3-Clause|GPL-2.0|GPL-3.0|MIT)
+            # this is an allowed license 
+            ;;
+        *)
+            echo "${program_name}: ERROR: ${license} is not one of the valid licenses, use -h to see the list"
+            exit 2
+    esac
+
     if [[ -z ${new_project_name} ]]; then
         echo "${program_name}: ERROR: missing command line argument: -n new_project_name, use -h for help"
         exit 2
     fi
+}
+
+function check_dependencies {
+    # Check for git
+    git --version > /dev/null || {
+       echo "${program_name}: ERROR: git is not installed in the PATH" 
+       exit 1
+    }
 }
 
 function create_project_directory {
@@ -127,6 +162,10 @@ function copy_test_data {
     }
 }
 
+function set_license {
+    cp ${new_project_name}/${git_tmp_dir}/license_options/${license} ${new_project_name}/LICENSE
+}
+
 function remove_biotool_repository {
     /bin/rm -fr "${new_project_name}/${git_tmp_dir}"
 }
@@ -157,6 +196,7 @@ function rename_project {
     find ${new_project_name} -name "*.temporary" -type f -delete
 }
 
+
 function create_project_repository {
     (
         cd ${new_project_name}
@@ -166,6 +206,7 @@ function create_project_repository {
     ) > /dev/null 2>&1
 }
 
+
 function verbose_message {
     if [ "${verbose}" = true ]; then
         echo "${program_name} $1"
@@ -174,24 +215,30 @@ function verbose_message {
 
 # 1. Parse command line arguments.
 parse_args $@
-# 2. Try to create new directory for the project.
+# 2. Check that dependencies are met
+verbose_message "checking for dependencies"
+check_dependencies
+# 3. Try to create new directory for the project.
 verbose_message "creating project directory ${new_project_name}"
 create_project_directory
-# 3. Clone biotool git repository into the newly created directory.
+# 4. Clone biotool git repository into the newly created directory.
 verbose_message "cloning biotool repository into ${new_project_name}/${git_tmp_dir}"
 clone_biotool_repository
-# 4. Recursively copy source tree from the git repository into the project directory.
+# 5. Recursively copy source tree from the git repository into the project directory.
 verbose_message "copying ${language} source tree into ${new_project_name}"
 copy_biotool_language
-# 5. Recursively copy test_data directory from the git repository into the project directory.
+# 6. Recursively copy test_data directory from the git repository into the project directory.
 verbose_message "copying test_data into ${new_project_name}"
 copy_test_data
-# 6. remove the cloned git repository.
-verbose_message "removing ${new_project_name}/${git_tmp_dir}"
-remove_biotool_repository
-# 7. Rename biotool to the new project name.
-verbose_message "renaming references to biotool to new project name ${new_project_name}" 
-rename_project
-# 8. Create repository for new project.
-verbose_message "initialising new git repository for ${new_project_name}"
-create_project_repository
+# 7. Set the license for the project
+verbose_message "setting the license to ${license}"
+set_license
+# 8. remove the cloned git repository.
+#verbose_message "removing ${new_project_name}/${git_tmp_dir}"
+#remove_biotool_repository
+# 9. Rename biotool to the new project name.
+#verbose_message "renaming references to biotool to new project name ${new_project_name}" 
+#rename_project
+# 10. Create repository for new project.
+#verbose_message "initialising new git repository for ${new_project_name}"
+#create_project_repository

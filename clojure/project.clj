e907a0d099dbe9c24ccf883ccdbca4614e7cc9d3
Bernie Pope <bjpope@unimelb.edu.au>
2017-03-06 19:46:14 +1100

    Updated metadata in Clojure project file

--- clojure/project.clj
+++ clojure/project.clj
@@ -1,8 +1,8 @@
 (defproject biotool "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "An example bioinformatics tool to demonstrate good programming practices"
+  :url "https://github.com/biotool-paper/biotool"
+  :license {:name "MIT"
+            :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [clj-biosequence "0.5.2"]
                  [org.clojure/tools.cli "0.3.5"]

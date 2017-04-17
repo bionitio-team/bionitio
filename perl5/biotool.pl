5fa74d077d1014ff7ed72bfa1bde15637f267e10
Bernie Pope <bjpope@unimelb.edu.au>
2017-04-17 12:15:55 +1000

    Add functions for main, get_options, init_logging, add logging to help message, remove some global variables

--- perl5/biotool.pl
+++ perl5/biotool.pl
@@ -16,78 +16,94 @@ use Log::Log4perl qw(get_logger :nowarn);
 my @COLUMNS = qw(FILENAME TOTAL NUMSEQ MIN AVG MAX);
 my(undef,undef,$EXE) = File::Spec->splitpath($0);
 my $VERSION = "1.0";
+my $DEFAULT_MINLEN = 0;
 my $verbose = 0;
-my $minlen = 0;
-my $logfile = "";
+my $logger;
 # Keep a copy of ARGV, so we can write the original to the log file
 my @ORIGINAL_ARGV = @ARGV;
+
 
 #.........................................................................
 # process command line 
 
-GetOptions(
-  "help"     => sub { usage(0) },
-  "version"  => sub { print "$EXE $VERSION\n"; exit(0); },
-  "verbose"  => sub { $verbose++ },
-  "minlen=i" => \$minlen,
-  "log=s" => \$logfile,
-)
-or usage(2);
+sub get_options {
+  my $minlen = 0;
+  my $logfile = "";
 
-# XXX this does not look portable!
-push @ARGV, "/dev/stdin" unless @ARGV;
+  GetOptions(
+    "help"     => sub { usage(0) },
+    "version"  => sub { print "$EXE $VERSION\n"; exit(0); },
+    "verbose"  => sub { $verbose++ },
+    "minlen=i" => \$minlen,
+    "log=s" => \$logfile,
+  )
+  or usage(2);
+  
+  # XXX this does not look portable!
+  push @ARGV, "/dev/stdin" unless @ARGV;
+
+  return (minlen => $minlen, logfile => $logfile);
+}
 
 #.........................................................................
 # initialise logging 
 
-# message pattern is:
-#   %d date (and time), %p priority (level), %m message, %n newline
-# minimum priority is INFO
+sub init_logging {
+  # message pattern is:
+  #   %d date (and time), %p priority (level), %m message, %n newline
+  # minimum priority is INFO
 
-if ($logfile ne "") {
-  my $log_conf = qq(
-    log4perl.logger                    = INFO, FileApp
-    log4perl.appender.FileApp          = Log::Log4perl::Appender::File
-    log4perl.appender.FileApp.filename = $logfile 
-    log4perl.appender.FileApp.layout   = PatternLayout
-    log4perl.appender.FileApp.layout.ConversionPattern = %d %p %m%n
-  );
-  Log::Log4perl->init( \$log_conf );
+  my($logfile) = @_;
+  
+  if ($logfile ne "") {
+    my $log_conf = qq(
+      log4perl.logger                    = INFO, FileApp
+      log4perl.appender.FileApp          = Log::Log4perl::Appender::File
+      log4perl.appender.FileApp.filename = $logfile 
+      log4perl.appender.FileApp.layout   = PatternLayout
+      log4perl.appender.FileApp.layout.ConversionPattern = %d %p %m%n
+    );
+    Log::Log4perl->init( \$log_conf );
+  }
+      
+  # Obtain a logger instance
+  $logger = get_logger("Biotool");
+  
+  $logger->info("program started");
+  # Log the program name and command line arguments
+  $logger->info("command line arguments: $0 @ORIGINAL_ARGV");
 }
-    
-# Obtain a logger instance
-my $logger = get_logger("Biotool");
-
-$logger->info("program started");
-# Log the program name and command line arguments
-$logger->info("command line arguments: $0 @ORIGINAL_ARGV");
 
 #.........................................................................
 # MAIN
 
-my $badfiles=0;
+sub main {
+  my %options = get_options();
+  init_logging($options{logfile});
 
-print tsv(\@COLUMNS);
-
-for my $file (@ARGV) {
-  #print STDERR "Processing: $file\n" if $verbose >= 1;
-  $logger->info("Processing FASTA file from $file");
-  unless (-r $file) {
-    # XXX should really have an exit_with_error function
-    # that can display error on STDERR and also write to log
-    print STDERR "ERROR: Unable to read file: $file\n";
-    $logger->error("ERROR: Unable to read file: $file");
-    exit(1);
+  my $badfiles=0;
+  
+  print tsv(\@COLUMNS);
+  
+  for my $file (@ARGV) {
+    $logger->info("Processing FASTA file from $file");
+    unless (-r $file) {
+      # XXX should really have an exit_with_error function
+      # that can display error on STDERR and also write to log
+      print STDERR "ERROR: Unable to read file: $file\n";
+      $logger->error("ERROR: Unable to read file: $file");
+      exit(1);
+    }
+    my $res = process_file($file, $options{minlen});
+    print tsv($res) if $res;
   }
-  my $res = process_file($file);
-  print tsv($res) if $res;
+  exit(0);
 }
-exit(0);
 
 #.........................................................................
 
 sub process_file {
-  my($fname) = @_;
+  my($fname, $minlen) = @_;
   
   # to collect stats
   my $bp=0;
@@ -135,7 +151,10 @@ Options:
   --help       Show this help
   --version    Print version and exit
   --verbose    Print more stuff about what's happening
-  --minlen N   Minimum length sequence to include in stats (default=$minlen)
+  --minlen N   Minimum length sequence to include in stats (default=$DEFAULT_MINLEN)
+  --log FILE   Log messages are written to FILE
 EOF
   exit $errcode;
 }
+
+main();

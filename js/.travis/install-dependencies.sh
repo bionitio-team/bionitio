377a7b2216180af6f5b4cc3732f3fef54d71303f
Peter Georgeson <peter@supernifty.org>
2017-06-04 00:06:20 +1000

    upgrade node

--- js/.travis/install-dependencies.sh
+++ js/.travis/install-dependencies.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-# Install Javascript dependencies 
-# Install node modules
+# Install npm
+# Install node 
 
 echo 'JS install'
 (
    sudo apt-get update && sudo apt-get install npm -y
    cd js
-   sudo npm install -g npm
+   curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+   sudo apt-get install -y nodejs
    sudo npm install -g npm
 )

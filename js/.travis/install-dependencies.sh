9c39d97e09c8e67ae4857c7ffe434e4c2e6df766
Peter Georgeson <peter@supernifty.org>
2017-06-04 21:49:00 +1000

    check node version

--- js/.travis/install-dependencies.sh
+++ js/.travis/install-dependencies.sh
@@ -7,5 +7,6 @@ echo 'JS install'
 (
    curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
    sudo apt-get install -y nodejs
-   sudo apt-get install build-essential
+   npm -v
+   nodejs -v
 )

5fe3351ce2cf8aa37dd2c86a805c38a421129625
Peter Georgeson <peter@supernifty.org>
2017-06-05 21:39:26 +1000

    node upgrade alt

--- js/.travis/install-dependencies.sh
+++ js/.travis/install-dependencies.sh
@@ -4,17 +4,15 @@
 # Install node 
 
 echo 'JS install'
+(
    sudo apt-get install -y build-essential libssl-dev nodejs
-   curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
-   export NVM_DIR="$HOME/.nvm"
-   [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-   nvm ls-remote
-   nvm install 8.0.0
-   nvm use 8.0.0
-   nvm alias default 8.0.0
+   sudo npm cache clean -f
+   sudo npm install -g n
+   sudo n stable
    npm -v
    node -v
    which node
    which nodejs
    cd js
    npm install
+)

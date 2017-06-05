750df394c4ad3b8384899fc3b7840aa3d890b679
Peter Georgeson <peter@supernifty.org>
2017-06-05 21:47:24 +1000

    travis node

--- js/.travis/install-dependencies.sh
+++ js/.travis/install-dependencies.sh
@@ -5,14 +5,8 @@
 
 echo 'JS install'
 (
-   sudo apt-get install -y build-essential libssl-dev nodejs
-   sudo npm cache clean -f
-   sudo npm install -g n
-   sudo n stable
    npm -v
    node -v
-   which node
-   which nodejs
    cd js
    npm install
 )

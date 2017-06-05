8752f67b0be4864ee11d215a6b4e5da68ff01625
Peter Georgeson <peter@supernifty.org>
2017-06-05 19:25:26 +1000

    use xenial image

--- js/.travis/install-dependencies.sh
+++ js/.travis/install-dependencies.sh
@@ -5,11 +5,7 @@
 
 echo 'JS install'
 (
-   curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
-   sudo apt-get install -y nodejs
-   npm -v
-   nodejs -v
-   node -v
+   sudo apt-get update && sudo apt-get install npm -y
    cd js
    npm install
 )

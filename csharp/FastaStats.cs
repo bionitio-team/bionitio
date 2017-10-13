12c2feffec46e0f83d225fa13db5fc1d60153185
Michael Milton <ttmigueltt@gmail.com>
2017-10-13 19:40:23 +1100

    Generally working CLI

---
+++ csharp/FastaStats.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Bio.IO.FastA;
+
+namespace csharp
+{
+    public class FastaStats
+    {
+        public long numSeqs;
+        public long numBases;
+        public long minLength;
+        public long maxLength;
+        public double average;
+
+        public FastaStats()
+        {
+        }
+
+        public override string ToString()
+        {
+            return $@"Number of Sequences: {numSeqs}
+Number of Bases: {numBases}
+Minimum Sequence Length: {minLength}
+Maximum Sequence Length: {maxLength}
+Average Sequence Length: {average}";
+        }
+
+        public static FastaStats Calculate(string filename)
+        {
+            var fileStream = new FileStream(filename, FileMode.Open);
+            return Calculate(fileStream);
+        }
+
+        public static FastaStats Calculate(FileStream stream)
+        {
+            var parser = new FastAParser();
+            var sequences = parser.Parse(stream);
+            return Calculate(sequences);
+        }
+
+        public static FastaStats Calculate(IEnumerable<Bio.ISequence> sequences)
+        {
+            return sequences
+                .GroupBy(sequence => "")
+                .Select(grouping => new FastaStats
+                {
+                    numSeqs = grouping.Count(),
+                    numBases = grouping.Sum(sequence => sequence.Count),
+                    minLength = grouping.Min(sequence => sequence.Count),
+                    maxLength = grouping.Min(sequence => sequence.Count),
+                    average = grouping.Average(sequence => sequence.Count)
+                }).First();
+        }
+    }
+}
--- csharp/Program.cs
+++ csharp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Microsoft.Extensions.CommandLineUtils;
+using Bio.IO.FastA;
 
 namespace csharp
 {
@@ -6,7 +8,32 @@ namespace csharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var cli = new CommandLineApplication
+            {
+                Name = "bionitio",
+                Description = "Print fasta stats"
+            };
+            
+            // Args
+            cli.HelpOption("-?|-h|--help");
+            var fastqs = cli.Argument("fastqs", "Input FASTA files", true);
+            var minlen = cli.Option("--minlen", "Minimum length sequence to include in stats (default 0)",
+                CommandOptionType.SingleValue);
+            var log = cli.Option("--log", "record program progress in LOG_FILE", CommandOptionType.SingleValue);
+            
+            // Parsing
+            cli.OnExecute(() =>
+            {
+                var parser = new FastAParser();
+                foreach (var fasta in fastqs.Values)
+                {
+                    var stats = FastaStats.Calculate(fasta);
+                    Console.WriteLine(stats);
+                }
+                return 0;
+            });
+
+            cli.Execute(args);
         }
     }
 }

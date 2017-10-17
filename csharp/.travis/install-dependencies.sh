1f2eb66f8efb5bdecf0c70bcbbbf6cd2488e0a41
Michael Milton <ttmigueltt@gmail.com>
2017-10-18 00:57:15 +1100

    Add bionitio install + test scripts

---
+++ csharp/.travis/install-dependencies.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Install dotnet
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-trusty-prod trusty main" > /etc/apt/sources.list.d/dotnetdev.list'
+apt-get update
+apt-get install dotnet-sdk-2.0.0
+
+# Compile for linux
+dotnet publish --runtime linux-x64

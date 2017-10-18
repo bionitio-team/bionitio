e0677ca149cb9dc472a6545ca7b4b4103a1ff0a8
Michael Milton <ttmigueltt@gmail.com>
2017-10-18 11:10:20 +1100

    Install in csharp dir

--- csharp/.travis/install-dependencies.sh
+++ csharp/.travis/install-dependencies.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+cd csharp
+
 # Install dotnet
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg

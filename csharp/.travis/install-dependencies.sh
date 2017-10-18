7a576325d96f4878166dc93059898646a3722a61
Michael Milton <ttmigueltt@gmail.com>
2017-10-18 11:02:51 +1100

    Use sudo in install script

--- csharp/.travis/install-dependencies.sh
+++ csharp/.travis/install-dependencies.sh
@@ -2,10 +2,10 @@
 
 # Install dotnet
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
-sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-trusty-prod trusty main" > /etc/apt/sources.list.d/dotnetdev.list'
-apt-get update
-apt-get install dotnet-sdk-2.0.0
+sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-trusty-prod trusty main" > /etc/apt/sources.list.d/dotnetdev.list'
+sudo apt-get update
+sudo apt-get install dotnet-sdk-2.0.0
 
 # Compile for linux
 dotnet publish --runtime linux-x64

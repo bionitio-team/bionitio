91f62b61a2019551c98407993f13ffec4475d189
Bernie Pope <bjpope@unimelb.edu.au>
2019-07-05 12:01:18 +1000

    Run the test suite

--- boot/bionitio-boot-test.sh
+++ boot/bionitio-boot-test.sh
@@ -8,6 +8,11 @@ BOOTSTRAP_SCRIPT=$1
 # Run the script and create a new python repository
 ${BOOTSTRAP_SCRIPT} -i python -n skynet -c BSD-3-Clause -a 'Miles Bennett Dyson' -e 'miles@cyberdyne.com' -l logfile
 
+# Install the new program into a virtual environment
 python3 -m venv skynet_dev
 source skynet_dev/bin/activate
 pip install -U skynet 
+
+# Run the test suite
+
+./skynet/functional_tests/skynet-test.sh -p bionitio -d ./skynet/functional_tests/test_data

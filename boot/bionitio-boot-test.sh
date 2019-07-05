603c0efa89f50c2ddd6fcda2529e59d6dcddcf34
Bernie Pope <bjpope@unimelb.edu.au>
2019-07-05 15:21:48 +1000

    Run bootstrap test script

--- boot/bionitio-boot-test.sh
+++ boot/bionitio-boot-test.sh
@@ -3,10 +3,10 @@
 # Test the bionitio bootstrap script
 # The path to the bootstrap script is passed in as the first argument
 
-BOOTSTRAP_SCRIPT=$1
+#BOOTSTRAP_SCRIPT=$1
 
 # Run the script and create a new python repository
-${BOOTSTRAP_SCRIPT} -i python -n biodemo -c BSD-3-Clause -a 'Example Author Name' -e 'example@email.address.com' -l logfile
+#${BOOTSTRAP_SCRIPT} -i python -n biodemo -c BSD-3-Clause -a 'Example Author Name' -e 'example@email.address.com' -l logfile
 
 # Install the new program into a virtual environment
 python3 -m venv biodemo_dev
